0fddda6e8fb5bdef6a2d852c9dd4ffc9a909240a
Rahul Dange <rahuldangeofficial@gmail.com>
2020-12-08 10:37:38 +0530

    NCER+ Application Android Client

---
+++ app/src/main/java/com/ncerplus/android/MainActivity.java
@@ -0,0 +1,91 @@
+package com.ncerplus.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://dbatubeta.blogspot.com/p/home.html");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Dexter.withActivity(MainActivity.this)
+                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                                request.setMimeType(mimetype);
+                                String cookies = CookieManager.getInstance().getCookie(url);
+                                request.addRequestHeader("cookie", cookies);
+                                request.addRequestHeader("User-Agent", userAgent);
+                                request.setDescription("Downloading File...");
+                                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(
+                                        Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                                url, contentDisposition, mimetype));
+
+                                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                downloadManager.enqueue(request);
+                                Toast.makeText(MainActivity.this, "Downloading File..", Toast.LENGTH_LONG).show();
+
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
